8a36ee18a28e5ed7aa2affaa259d4a60d8c6ce0a
Michael Strobel <michael@kryops.de>
2021-05-08 11:25:31 +0200

    Add React example using create-react-app

---
+++ examples/react-app/src/App.js
@@ -0,0 +1,28 @@
+import toReact from 'svelte-adapter/react';
+import { PdfmakeHtmlRenderer } from 'pdfmake-html-renderer'
+
+import 'pdfmake-html-renderer/dist/index.css';
+
+const Renderer = toReact(PdfmakeHtmlRenderer) 
+
+function App() {
+  return (
+    <div>
+      <Renderer
+        document={{
+          content: [
+            {
+              text: 'pdfmake-html-renderer React Example',
+              fontSize: 18,
+              bold: true
+            },
+            '\n',
+            'Hello, world!',
+          ]
+        }}
+      />
+    </div>
+  );
+}
+
+export default App;
