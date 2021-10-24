862595ed1f95d868f6bb0b835b03f289d2c92d64
Michael Strobel <michael@kryops.de>
2021-10-24 12:12:20 +0200

    Upgrade dev dependencies

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,7 @@ import type { Config } from '@jest/types'
 const config: Config.InitialOptions = {
   preset: 'jest-puppeteer',
   testTimeout: 20000,
+  transformIgnorePatterns: ['/node_modules/(?!(get-port)/)'],
 }
 
 export default config
