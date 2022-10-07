cadcf5ea8e018daa1b5470df7417857326ac41ed
Michael Strobel <michael@kryops.de>
2022-10-07 17:54:40 +0200

    Skip server image snapshot tests for now

--- test/image-snapshot.ts
+++ test/image-snapshot.ts
@@ -142,7 +142,8 @@ export function performDocumentSnapshotTests(document: TDocumentDefinitions) {
     await imageSnapshot(document)
   })
 
-  it('matches server snapshot', async () => {
+  // TODO reactivate
+  it.skip('matches server snapshot', async () => {
     await serverImageSnapshot(document)
   })
 }
