5c12d5c41f65bf16f7873b7ae4f8b708a851ad98
Michael Manzinger <michael.manzinger@mobilex.eu>
2023-10-04 11:04:46 +0200

    Add SvelteKit example (#4)

---
+++ examples/sveltekit/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import PdfmakeHtmlRenderer from 'pdfmake-html-renderer'
+</script>
+
+<PdfmakeHtmlRenderer
+  document={{
+    content: [
+      {
+        text: 'pdfmake-html-renderer React Example',
+        fontSize: 18,
+        bold: true,
+      },
+      '\n',
+      'Hello, world!',
+    ],
+  }}
+/>
