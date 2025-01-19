78fdedecf6c5a93f07eb4db24e218ad1c4430dd6
Michael Manzinger <michael@kryops.de>
2025-01-19 14:25:34 +0100

    Fix warning about tr being a direct child of table

--- src/components/ContentTableRenderer.svelte
+++ src/components/ContentTableRenderer.svelte
@@ -29,26 +29,28 @@
     node.layout === 'headerLineOnly' ||
     node.layout === 'lightHorizontalLines'}
 >
-  {#each node.table?.body ?? [] as tr, rowIndex}
-    <tr
-      class:phr-header={(node.table.headerRows ?? -1) === rowIndex + 1 &&
-        rowIndex < (node.table?.body.length ?? 0) - 1}
-    >
-      {#each tr as td, columnIndex}
-        {#if shouldRenderCell(td, rowIndex, columnIndex, coveredCells)}
-          <td
-            colspan={td.colSpan}
-            rowspan={td.rowSpan}
-            style={getTableCellStyleString(td, node, rowIndex, columnIndex)}
-          >
-            {#if isActualTableCell(td)}
-              <ContentRenderer node={td} />
-            {/if}
-          </td>
-        {/if}
-      {/each}
-    </tr>
-  {/each}
+  <tbody>
+    {#each node.table?.body ?? [] as tr, rowIndex}
+      <tr
+        class:phr-header={(node.table.headerRows ?? -1) === rowIndex + 1 &&
+          rowIndex < (node.table?.body.length ?? 0) - 1}
+      >
+        {#each tr as td, columnIndex}
+          {#if shouldRenderCell(td, rowIndex, columnIndex, coveredCells)}
+            <td
+              colspan={td.colSpan}
+              rowspan={td.rowSpan}
+              style={getTableCellStyleString(td, node, rowIndex, columnIndex)}
+            >
+              {#if isActualTableCell(td)}
+                <ContentRenderer node={td} />
+              {/if}
+            </td>
+          {/if}
+        {/each}
+      </tr>
+    {/each}
+  </tbody>
 </table>
 
 <style>
@@ -65,32 +67,32 @@
 
   /* table layouts */
 
-  .phr-noSidePadding > tr > td:first-child {
+  .phr-noSidePadding > tbody > tr > td:first-child {
     padding-left: 0;
   }
-  .phr-noSidePadding > tr > td:last-child {
+  .phr-noSidePadding > tbody > tr > td:last-child {
     padding-right: 0;
   }
 
-  .phr-noBorders > tr > td {
+  .phr-noBorders > tbody > tr > td {
     border: none;
   }
 
-  .phr-headerLineOnly > .phr-header {
+  .phr-headerLineOnly > tbody > .phr-header {
     border-bottom: 2pt solid #000;
   }
 
-  .phr-headerLineOnly > tr > td {
+  .phr-headerLineOnly > tbody > tr > td {
     border: none;
     padding-left: 8pt;
     padding-right: 8pt;
   }
 
-  .phr-lightHorizontalLines > .phr-header > td {
+  .phr-lightHorizontalLines > tbody > .phr-header > td {
     border-bottom-width: 2pt;
   }
 
-  .phr-lightHorizontalLines > tr > td {
+  .phr-lightHorizontalLines > tbody > tr > td {
     border-left: none;
     border-right: none;
     border-top: none;
@@ -99,11 +101,11 @@
     padding-right: 8pt;
   }
 
-  .phr-lightHorizontalLines > tr:first-child > td {
+  .phr-lightHorizontalLines > tbody > tr:first-child > td {
     border-bottom-color: #000;
   }
 
-  .phr-lightHorizontalLines > tr:last-child > td {
+  .phr-lightHorizontalLines > tbody > tr:last-child > td {
     border-bottom: none;
   }
 </style>
