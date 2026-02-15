4394e3557902d24a352ca873293d995a7a26e1dd
Michael Manzinger <michael@kryops.de>
2026-02-15 13:36:11 +0100

    Update to pdfmake 0.3.4

--- pdfmake-types.d.ts
+++ pdfmake-types.d.ts
@@ -2,18 +2,6 @@ import { Style, TableOfContent, TableCellProperties } from 'pdfmake/interfaces'
 
 // Types updates not yet released to DefinitelyTyped
 declare module 'pdfmake/interfaces' {
-  interface TableOfContent {
-    hideEmpty?: boolean | undefined
-    sortBy?: 'title' | 'page' | undefined
-    sortLocale?: string | undefined
-  }
-
-  interface Style {
-    decorationThickness?: number | undefined
-    // cannot actually exist on all styles, only on the ones in the styles dictionary
-    extends?: string | string[] | undefined
-  }
-
   interface TableCellProperties {
     verticalAlignment?: 'top' | 'middle' | 'bottom' | undefined
   }
